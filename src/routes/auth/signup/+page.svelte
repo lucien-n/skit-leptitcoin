dfcdac0af5a74ec0cc141834fee842dbe87cd256
lucien-neuhoff <77048269+lucien-neuhoff@users.noreply.github.com>
2023-06-14 11:30:59 +0200

    ref: removed svgs from html in signup to use the file ones

--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -2,6 +2,8 @@
 	import { goto } from '$app/navigation';
 	import { supabase } from '$lib/supabase';
 	import { toastStore } from '@skeletonlabs/skeleton';
+	import closedEyeSvg from '$lib/assets/eye-closed.svg?raw';
+	import openedEyeSvg from '$lib/assets/eye-opened.svg?raw';
 
 	let showPassword: boolean = false;
 
@@ -106,38 +108,9 @@
 					on:click={() => (showPassword = !showPassword)}
 				>
 					{#if showPassword}
-						<svg
-							data-testid="geist-icon"
-							fill="none"
-							height="24"
-							shape-rendering="geometricPrecision"
-							stroke="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="1.5"
-							viewBox="0 0 24 24"
-							width="24"
-							style="color:var(--geist-foreground)"
-							><path
-								d="M17.94 17.94A10.07 10.07 0 0112 20c-7 0-11-8-11-8a18.45 18.45 0 015.06-5.94M9.9 4.24A9.12 9.12 0 0112 4c7 0 11 8 11 8a18.5 18.5 0 01-2.16 3.19m-6.72-1.07a3 3 0 11-4.24-4.24"
-							/><path d="M1 1l22 22" /></svg
-						>{:else}
-						<svg
-							data-testid="geist-icon"
-							fill="none"
-							height="24"
-							shape-rendering="geometricPrecision"
-							stroke="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="1.5"
-							viewBox="0 0 24 24"
-							width="24"
-							style="color:var(--geist-foreground)"
-							><path
-								d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"
-							/><circle cx="12" cy="12" r="3" /></svg
-						>
+						{@html closedEyeSvg}
+					{:else}
+						{@html openedEyeSvg}
 					{/if}
 				</button>
 			</div>
