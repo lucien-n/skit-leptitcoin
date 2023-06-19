5384d46829f3070b3e10e2888225e1d3500e062a
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2023-06-19 10:12:03 +0200

    feat: rating component

---
+++ src/lib/components/Rating.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import StarSvg from '$lib/assets/star.svg?raw';
+	import StarFilledSvg from '$lib/assets/star-filled.svg?raw';
+	import { createEventDispatcher } from 'svelte';
+
+	export let rating: number;
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<section id="rating" class="flex">
+	{#each { length: 5 } as _, i}
+		<button
+			on:click={() => {
+				rating = i + 1;
+				dispatch('click');
+			}}
+		>
+			{#if rating > i}
+				{@html StarFilledSvg}
+			{:else}
+				{@html StarSvg}
+			{/if}
+		</button>
+	{/each}
+</section>
