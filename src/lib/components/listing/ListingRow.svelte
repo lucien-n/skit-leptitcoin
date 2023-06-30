62c8bb29677f1956358d9ca19c6fd4a3fc3cdc6b
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-06-30 16:25:16 +0200

    lib: propper prettier config - wip: listing validation

--- src/lib/components/listing/ListingRow.svelte
+++ src/lib/components/listing/ListingRow.svelte
@@ -1,61 +1,62 @@
 <script lang="ts">
-  import { formatDate } from "$lib/helper";
-  import { supaUserStore, userStore } from "$lib/store";
-  import type { SupaListing } from "$lib/types/supa_listing";
-  import DeleteButton from "$lib/components/widgets/DeleteButton.svelte";
-  import EditButton from "$lib/components/widgets/EditButton.svelte";
-  import ConditionBadge from "$lib/components/listing/ConditionBadge.svelte";
+	import { formatDate } from '$lib/helper';
+	import { supaUserStore, userStore } from '$lib/store';
+	import type { SupaListing } from '$lib/types/supa_listing';
+	import DeleteButton from '$lib/components/widgets/DeleteButton.svelte';
+	import EditButton from '$lib/components/widgets/EditButton.svelte';
+	import ConditionBadge from '$lib/components/listing/ConditionBadge.svelte';
+	import ValidateButton from '../widgets/ValidateButton.svelte';
 
-  export let listing: SupaListing;
+	export let listing: SupaListing;
 
-  const maxDescriptionLength: number = 30;
+	const maxDescriptionLength: number = 30;
 </script>
 
 <div class="card p-2 h-14 flex">
-  <a
-    href="/l/{listing.uid}"
-    class="flex items-center w-full justify-between group/listing-row"
-  >
-    <div class="flex items-end gap-1 group-hover/listing-row:underline">
-      <h4 class="h4">
-        {listing.title}
-      </h4>
-      <p class="font-normal">
-        {listing.description.length > maxDescriptionLength
-          ? listing.description.slice(0, maxDescriptionLength) + "..."
-          : listing.description}
-      </p>
-    </div>
-    <div class="flex gap-2">
-      <ConditionBadge condition={listing.condition} />
-    </div>
-    <div class="flex items-center gap-2">
-      <p class="italic opacity-70">
-        {formatDate(listing.createdAt || new Date().getTime())}
-      </p>
-      <div class="badge italic variant-ghost text-base">
-        {listing.price} €
-      </div>
-      <!-- edit & delete - only visible to author & admins -->
-      <div
-        class:hidden={listing.author_uid !== $userStore?.id &&
-          $supaUserStore &&
-          $supaUserStore.role % 8 !== 0}
-      />
-    </div>
-  </a>
-  <div
-    class="flex self-end gap-2"
-    class:hidden={$supaUserStore &&
-      $supaUserStore.uid !== listing.author_uid &&
-      $supaUserStore?.role < 8}
-  >
-    <EditButton listing_uid={listing.uid} />
-    <DeleteButton
-      listing_uid={listing.uid}
-      on:click={() => {
-        // window.location.href = window.location.href;
-      }}
-    />
-  </div>
+	<a href="/l/{listing.uid}" class="flex items-center w-full justify-between group/listing-row">
+		<div class="flex items-end gap-1 group-hover/listing-row:underline">
+			<h4 class="h4">
+				{listing.title}
+			</h4>
+			<p class="font-normal">
+				{listing.description.length > maxDescriptionLength
+					? listing.description.slice(0, maxDescriptionLength) + '...'
+					: listing.description}
+			</p>
+		</div>
+		<div class="flex gap-2">
+			<ConditionBadge condition={listing.condition} />
+		</div>
+		<div class="flex items-center gap-2">
+			<p class="italic opacity-70">
+				{formatDate(listing.createdAt || new Date().getTime())}
+			</p>
+			<div class="badge italic variant-ghost text-base">
+				{listing.price} €
+			</div>
+			<!-- edit & delete - only visible to author & admins -->
+			<div
+				class:hidden={listing.author_uid !== $userStore?.id &&
+					$supaUserStore &&
+					$supaUserStore.role % 8 !== 0}
+			/>
+		</div>
+	</a>
+	<div
+		class="flex self-end gap-2"
+		class:hidden={$supaUserStore &&
+			$supaUserStore.uid !== listing.author_uid &&
+			$supaUserStore?.role < 8}
+	>
+		<EditButton listing_uid={listing.uid} />
+		<DeleteButton
+			listing_uid={listing.uid}
+			on:click={() => {
+				// window.location.href = window.location.href;
+			}}
+		/>
+		{#if $supaUserStore && $supaUserStore?.role >= 8}
+			<ValidateButton listing_uid={listing.uid} />
+		{/if}
+	</div>
 </div>
---
+++ src/lib/components/widgets/ValidateButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import CheckSvg from '$lib/components/svgs/CheckSvg.svelte';
+
+	export let listing_uid: string;
+	export let index: number = 0;
+
+	const dispatch = createEventDispatcher();
+
+	const validateListing = () => {
+		fetch(`/d/${listing_uid}`);
+		dispatch('click');
+	};
+</script>
+
+<button
+	on:click={validateListing}
+	class="variant-success-error btn p-2"
+	aria-label="validate"
+	tabindex={index}
+>
+	<CheckSvg />
+</button>
