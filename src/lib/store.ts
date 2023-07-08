dde5141aeab2eb389b1148ebb4db9838c9449d00
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-07-08 17:43:37 +0200

    ref: removed unused reload

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,7 +3,7 @@ import type { SupaListing } from '$lib/types/supa_listing';
 import type { User } from '@supabase/supabase-js';
 import type { SupaUser } from '$lib/types/supa_user';
 
-export const searchStore: Writable<SearchParams> = writable({ reload: 0 });
+export const searchStore: Writable<SearchParams> = writable({});
 export const listingsStore: Writable<SupaListing[]> = writable([]);
 export const userStore: Writable<User | null> = writable();
 export const supaUserStore: Writable<SupaUser | undefined | null> = writable();
--- src/lib/types/search_params.ts
+++ src/lib/types/search_params.ts
@@ -1,7 +1,6 @@
 type SearchParams = {
-  search?: string;
-  category?: string;
-  price_min?: number;
-  price_max?: number;
-  reload: number;
+	search?: string;
+	category?: string;
+	price_min?: number;
+	price_max?: number;
 };
