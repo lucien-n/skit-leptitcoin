352b4286f374c91625da8afcf53d2bbf8c9d7383
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-07-11 23:28:15 +0200

    feat: Icon component & python script to generate it

--- src/lib/components/svgs/generate_icon.svelte.py
+++ src/lib/components/svgs/generate_icon.svelte.py
@@ -1,22 +1,54 @@
 import os
 import re
+import sys
 
 IMPORT_PREFIX = "$lib/components/svgs/"
 
 
 def main():
+    args = sys.argv
+    if len(args) < 2:
+        print("Please specify the svgs folder path")
+        return
+    svgs_path = args[1]
+
     imports = []
     icons = {}
 
-    for file in os.listdir():
+    for file in os.listdir(svgs_path):
         if not file.endswith("Svg.svelte"):
-            return
-
+            continue
         import_path = IMPORT_PREFIX + file
 
-        name = file.replace("Svg.svelte", "")
-        name = re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
-        print(name)
+        component_name = file.replace(".svelte", "")
+
+        icon_name = file.replace("Svg.svelte", "")
+        icon_name = re.sub(r"(?<!^)(?=[A-Z])", "_", icon_name).lower()
+
+        imports.append(f"import {component_name} from '{import_path}'")
+        icons[icon_name] = component_name
+
+    open(svgs_path + "/Icon.svelte", "w+")
+    with open(svgs_path + "/Icon.svelte", "a") as f:
+        f.write('<script lang="ts">\n')
+        for import_path in imports:
+            f.write(f"   {import_path}\n")
+        f.write("\n")
+        f.write("   export let name: string;\n")
+        f.write("\n")
+        f.write("    const ICONS = {\n")
+        for name in icons:
+            f.write(f"        {name}: {icons[name]},\n".strip("'"))
+        f.write("    }\n")
+        f.write("\n")
+        f.write(f"    const Icon = ICONS[name];\n")
+        f.write(f"</script>\n")
+        f.write("\n")
+        f.write("{#if Icon}\n")
+        f.write("    <Icon />\n")
+        f.write("{:else}\n")
+        f.write("    <span>Icon not found</span>\n")
+        f.write("{/if}")
 
 
 if __name__ == "__main__":
