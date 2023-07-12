57815703707b95450bf16a76086f3cff95bbb531
lucien-neuhoff <lucien.neuhoff@protonmail.com>
2023-07-12 15:26:41 +0200

    ref: tests tweaks

--- cypress/e2e/create-listings.cy.js
+++ cypress/e2e/create-listings.cy.js
@@ -1,6 +1,12 @@
 const LISTINGS = [
-	{ title: 'iPhone 12 Pro', description: 'Brand new iPhone 12 Pro, never used.' },
-	{ title: 'Nike Air Max 90', description: 'Limited edition Nike Air Max 90 sneakers.' },
+	{
+		title: 'iPhone 12 Pro',
+		description: 'Brand new iPhone 12 Pro, never used.'
+	},
+	{
+		title: 'Nike Air Max 90',
+		description: 'Limited edition Nike Air Max 90 sneakers.'
+	},
 	{
 		title: 'Modern Apartment in City Center',
 		description: 'Spacious apartment in the heart of the city.'
@@ -13,8 +19,14 @@ const LISTINGS = [
 		title: 'Vintage Vinyl Record Collection',
 		description: 'Collection of classic vinyl records in excellent condition.'
 	},
-	{ title: 'Mountain Bike', description: 'Full-suspension mountain bike for off-road adventures.' },
-	{ title: 'Designer Handbag', description: 'Luxury designer handbag, perfect for any occasion.' },
+	{
+		title: 'Mountain Bike',
+		description: 'Full-suspension mountain bike for off-road adventures.'
+	},
+	{
+		title: 'Designer Handbag',
+		description: 'Luxury designer handbag, perfect for any occasion.'
+	},
 	{
 		title: 'Cozy Studio Apartment',
 		description: 'Comfortable studio apartment with modern amenities.'
@@ -23,7 +35,10 @@ const LISTINGS = [
 		title: 'Gaming Laptop',
 		description: 'Powerful gaming laptop for immersive gaming experiences.'
 	},
-	{ title: 'Electric Guitar', description: 'Electric guitar for musicians and music lovers.' }
+	{
+		title: 'Electric Guitar',
+		description: 'Electric guitar for musicians and music lovers.'
+	}
 ];
 
 const CONDITIONS = [
@@ -35,32 +50,41 @@ const CONDITIONS = [
 ];
 const CATEGORIES = ['Vehicles', 'Fashion', 'Housing', 'Multimedia', 'Recreational'];
 
-const USER_NUM = Math.floor(Math.random() * 3);
+const NUMBER_OF_USERS = 4
+const NUMBER_OF_LISTINGS = 10
+
+const USER_NUM = Math.floor(Math.random() * NUMBER_OF_USERS);
 
 describe('Create listing', () => {
 	it('should signin & create a listing', () => {
-		cy.visit('/auth/signin');
+		for (let i = 0; i < NUMBER_OF_LISTINGS; i++) {
+			cy.visit('/auth/signin');
 
-		cy.get('#email').type('test' + USER_NUM + '@mail.com', { force: true });
-		cy.get('#password').type('test' + USER_NUM + '$', { force: true });
+			cy.get('#email').type('test' + USER_NUM + '@mail.com', { force: true });
+			cy.get('#password').type('test' + USER_NUM + '$', { force: true });
 
-		cy.get('#submit').click({ force: true });
+			cy.get('#submit').click({ force: true });
 
-		cy.visit('/new');
+			cy.visit('/new');
 
-		const LISTING = LISTINGS[Math.floor(Math.random() * Object.keys(LISTINGS).length)];
-		const PRICE = Math.floor(Math.random() * 10_000);
-		const CATEGORY_INDEX = Math.floor(Math.random() * CATEGORIES.length);
-		const CONDITION_INDEX = Math.floor(Math.random() * CONDITIONS.length);
+			cy.wait(1_000)
 
-		cy.get('#title').type(LISTING.title, { force: true });
-		cy.get('#description').type(LISTING.description, { force: true });
-		cy.get('#price').type(PRICE, { force: true });
-		cy.get('#category').select(CATEGORY_INDEX, { force: true });
-		cy.get(`:nth-child(${CONDITION_INDEX + 1}) > [data-testid="radio-item"]`).click({
-			force: true
-		});
+			const LISTING = LISTINGS[Math.floor(Math.random() * Object.keys(LISTINGS).length)];
+			const PRICE = Math.floor(Math.random() * 10_000);
+			const CATEGORY_INDEX = Math.floor(Math.random() * CATEGORIES.length);
+			const CONDITION_INDEX = Math.floor(Math.random() * CONDITIONS.length);
 
-		cy.get('#submit').click({ force: true });
+			cy.get('#title').type(LISTING.title, { force: true });
+			cy.get('#description').type(LISTING.description, { force: true });
+			cy.get('#price').type(PRICE, { force: true });
+			cy.get('#category').select(CATEGORY_INDEX, { force: true });
+			cy.get(`:nth-child(${CONDITION_INDEX + 1}) > [data-testid="radio-item"]`).click({
+				force: true
+			});
+
+			cy.wait(1_000)
+
+			cy.get('#submit').click({ force: true });
+		}
 	});
 });
