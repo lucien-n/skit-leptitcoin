229464f7e18dfa38ec5915c5c1c6171eb6ae0a7d
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-07-24 23:58:51 +0200

    wip: rewriting supabase.ts & testing it

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,21 @@
-import { describe, expect, it } from 'vitest';
+import { getListing, getListings } from '$lib/supabase';
+import { assert, describe, it } from 'vitest';
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
+describe('get listings', () => {
+	it('fetches 2 non-validated listings where category', async () => {
+		const resp = await getListings(1, 0, { is_validated: false, category: 'misc' });
+
+		assert.exists(resp);
+		assert.isArray(resp);
+		if (!resp) return;
+		assert.equal(resp.length, 2);
+	});
+
+	it('fetch validated listing where author_uid', async () => {
+		const resp = await getListing({ author_uid: 'ae86c595-e879-4125-8d13-a3734122e2bc' });
+
+		assert.exists(resp);
+		assert.isNotArray(resp);
+		if (!resp) return;
 	});
 });
