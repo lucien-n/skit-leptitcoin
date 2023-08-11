75c74e4ec224620c16d58dcca262ad4c34d5449e
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-08-11 16:53:17 +0200

    fix: card ghost proper display

--- src/lib/components/user/UserCardGhost.svelte
+++ src/lib/components/user/UserCardGhost.svelte
@@ -1,4 +1,8 @@
-<div class="flex gap-3 hover:underline">
+<script lang="ts">
+	export let as_card: boolean = true;
+</script>
+
+<div class="flex gap-3 hover:underline w-full md:w-fit" class:card={as_card} class:p-5={as_card}>
 	<div class="placeholder-circle w-16 animate-pulse" />
 	<div class="flex animate-pulse flex-col gap-2">
 		<div class="placeholder w-32 self-center" />
